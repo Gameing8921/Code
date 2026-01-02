6401b08665197680ee19c6866052aa733b344ddb
Gameing_8921 <flip.from.2013@gmail.com>
2026-01-02 21:24:27 +0000

    Further update to lesson 1.1 ig

--- C++ (LEARNING)/Lesson 1.1/main.cpp
+++ C++ (LEARNING)/Lesson 1.1/main.cpp
@@ -2,10 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello world!";
-    std::cout << "So, this is C++, nice";
-    std::cout << "For anyone checking, im following the learncpp.com guide";
-    std::cout << "I also forgot to add semicolons, its fixed now \nhow stupid i am";
-    std::cout << "\nits also weird how i need to add '/n' (uknowwhatimean) at the end of every stdcout, or newline doesnt happen"; // For anyone reading the code tell me how
+    std::cout << "Hello world\n!";
+    std::cout << "So, this is C++, nice\n";
+    std::cout << "For anyone checking, im following the learncpp.com guide\n";
+    std::cout << "I also forgot to add semicolons, its fixed now \nhow stupid i am\n";
+    std::cout << "its also weird how i need to add '/n' (uknowwhatimean) at the end of every stdcout, or newline doesnt happen\n";
+    std::cout << "i also love how i can use ctl+/ to comment out a line (but ill prob forget cuz in the pt layout its ctrl+shift+7)";
     return 0;
 }
