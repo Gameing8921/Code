0df346b53e29bc4aabcaabea263954a7ad7e8606
Gameing_8921 <flip.from.2013@gmail.com>
2026-01-02 21:12:40 +0000

    Added new lesson code

---
+++ C++ (LEARNING)/Lesson 1.1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!";
+    std::cout << "So, this is C++, nice";
+    std::cout << "For anyone checking, im following the learncpp.com guide";
+    std::cout << "I also forgot to add semicolons, its fixed now \nhow stupid i am";
+    std::cout << "\nits also weird how i need to add '/n' (uknowwhatimean) at the end of every stdcout, or newline doesnt happen"; // For anyone reading the code tell me how
+    return 0;
+}
