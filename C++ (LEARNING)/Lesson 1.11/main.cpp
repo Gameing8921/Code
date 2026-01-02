8bc3c1381a6729f980fe20c6d7e07585c0f44478
Gameing_8921 <flip.from.2013@gmail.com>
2026-01-02 23:41:46 +0000

    Added their (learncpp) solution to lesson 1.11

--- C++ (LEARNING)/Lesson 1.11/main.cpp
+++ C++ (LEARNING)/Lesson 1.11/main.cpp
@@ -9,11 +9,16 @@ int mySolution()
     return numberToMultiply * 2;
 }
 
-// int theirSolution()
-// {
-// }
+int theirSolution()
+{
+    std::cout << "Enter an integer: ";
+
+    int num{};       // define variable num as an integer variable
+    std::cin >> num; // get integer value from user's keyboard
+}
 
 int main()
 {
     std::cout << mySolution();
+    theirSolution();
 }
